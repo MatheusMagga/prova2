f02683a5fa4b82af926297252ee2dcd1a0242a67
MatheusMagga <41812143+MatheusMagga@users.noreply.github.com>
2019-10-01 09:34:47 -0300

    Add files via upload

---
+++ questao2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#define TAM 20
+char alfa[] = "abcdefghijklmnopqrstuvwxyzç";
+
+void main()
+{
+    char vetor[TAM];
+    int aux[TAM];
+    int temp = 0;
+
+    printf("Insira um palavra de no máximo 20 caracteres! \n");
+    gets(vetor);
+    //For para descobrir posições da palavra.
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < 28; j++)
+        {
+            if (alfa[j] == vetor[i])
+            {
+                aux[i] = j + 1;
+            }
+        }
+    }
+    //For para ordenar usando bubblesort.
+    for (int i = 1; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM - 1; j++)
+        {
+            if (aux[j] > aux[j + 1])
+            {
+                temp = aux[j];
+                aux[j] = aux[j + 1];
+                aux[j + 1] = temp;
+            }
+        }
+    }
+    //Print da nova ordem.
+    for (int i = 0; i < strlen(aux); i++)
+    {
+        printf("%d", aux[i]);
+    }
+    printf("\n");
+    printf("Sei que há algum erro de lógica, mas considere algo ai professor, please! (ツ)\n");
+}
